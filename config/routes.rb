f9cab00a60a5bb169864f2e9e967132420f804fd
arisson <arisson@pop-os.localdomain>
2021-05-08 23:39:55 -0400

    adição de rotas para o admin e home

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'home/index'
+  get 'admin/index'
+  devise_for :users
+
+
+  root :to => "home#index"
+
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
