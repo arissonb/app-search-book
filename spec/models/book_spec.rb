f540942898626f29391b7f6961486edebf187b77
Arisson <arissonbrasil000@gmail.com>
2021-05-11 01:10:09 -0400

    Configuração e testes de validação com rspec

---
+++ spec/models/book_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+RSpec.describe Book, :type => :model do
+
+  #Variavel temporario para validação de atributo, o error é acionado quando o valor é passado como nulo
+  subject {
+   described_class.new(title: "Anything",
+                       author: "Lorem ipsum",
+                       description: "Teste",
+                       status: "teste"
+                      )
+ }
+
+ it "is valid with valid attributes" do
+    expect(subject).to be_valid
+ end
+
+ #Teste para validar o campo title
+ it "is not valid without a title" do
+    subject.title = nil
+    expect(subject).to_not be_valid
+ end
+
+ #Teste para validar o campo author
+ it "is not valid without a author" do
+    subject.author = nil
+    expect(subject).to_not be_valid
+ end
+
+ #Teste para validar o campo description
+ it "is not valid without a description" do
+    subject.description = nil
+    expect(subject).to_not be_valid
+ end
+
+ #Teste para validar o campo status
+ it "is not valid without a status" do
+    subject.status = nil
+    expect(subject).to_not be_valid
+ end
+
+end
