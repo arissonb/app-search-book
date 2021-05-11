e43ccb9fc4d07307a15f2b4c7f43d0e1953a6959
Arisson <arissonbrasil000@gmail.com>
2021-05-11 10:34:34 -0400

    Validação de autenticação do admin

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,6 +1,20 @@
 class AdminController < ApplicationController
+
   #Help do devise para autenticação do usuario na controller Admin
-  before_action :authenticate_user!
-  #Layout para autenticação do usuario
+  before_action :authenticate_user!, :check_login_user
+  #Layout para autenticação do usuario admin
   layout 'admin'
+
+  protected
+
+  #Verificação de perfil de usuario
+  def check_login_user
+    if user_signed_in?
+      if current_user.inativo? || current_user.comum?
+        Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name)
+        redirect_to root_path
+      end
+    end
+  end
+
 end
