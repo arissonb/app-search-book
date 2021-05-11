728b7ae7cfff49315e6266413ad2f20a1ea9f077
Arisson <arissonbrasil000@gmail.com>
2021-05-11 01:18:47 -0400

    Alterações em arquivos do layout admin

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,4 +1,6 @@
 class AdminController < ApplicationController
-  def index
-  end
+  #Help do devise para autenticação do usuario na controller Admin
+  before_action :authenticate_user!
+  #Layout para autenticação do usuario
+  layout 'admin'
 end
---
+++ app/javascript/packs/admin.js
@@ -0,0 +1,8 @@
+
+import "../packs/config/util"
+import "../packs/config/modules"
+
+
+$(document).on('turbolinks:load', function () {
+
+})
